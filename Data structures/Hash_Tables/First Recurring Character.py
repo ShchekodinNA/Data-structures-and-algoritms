13dfee150773e176dff17f707bc0ffe47bf20936
ShchekodinNA <kacpercku1971@gmail.com>
2022-07-23 15:46:50 +0300

    Data Structure - Hash tables

--- Data structures/Hash_Tables/First Recurring Character.py
+++ Data structures/Hash_Tables/First Recurring Character.py
@@ -1,21 +1,22 @@
-#Google Question
-#Given an array = [2,5,1,2,3,5,1,2,4]:
-#It should return 2
+# Google Question
+# Given an array = [2,5,1,2,3,5,1,2,4]:
+# It should return 2
 
-#Given an array = [2,1,1,2,3,5,1,2,4]:
-#It should return 1
+# Given an array = [2,1,1,2,3,5,1,2,4]:
+# It should return 1
 
-#Given an array = [2,3,4,5]:
-#It should return undefined
+# Given an array = [2,3,4,5]:
+# It should return undefined
+
+# Bonus... What if we had this:
+# [2,5,5,2,3,5,1,2,4]
+# return 5 because the pairs are before 2,2
 
 t_undef = 'undefined'
 
-def first_recurring_character(lst: list):
-    if len(lst) < 2:
-        return None
 
+def first_recurring_character(lst: list):   # O(n)
     htbl = {}
-
     for i in range(len(lst)):
         if lst[i] in htbl:
             return lst[i]
@@ -23,10 +24,4 @@ def first_recurring_character(lst: list):
     return t_undef
 
 
-
-print(first_recurring_character([2,3,4,5]))
-
-
-#Bonus... What if we had this:
-# [2,5,5,2,3,5,1,2,4]
-# return 5 because the pairs are before 2,2
+print(first_recurring_character([2]))
